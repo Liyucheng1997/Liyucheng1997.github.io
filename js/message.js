9de985f7a28579381be5ba58f0b6ed313ad91f1f
Liyucheng1997 <63134475+Liyucheng1997@users.noreply.github.com>
2020-04-12 11:30:48 +0800

    1

---
+++ js/message.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    var tet= document.getElementById("text");
+    var butt= document.getElementById("btn");
+    var tet_li= document.getElementById("text_li");
+ 
+    butt.onclick=function msg(){
+       var Li= document.createElement("li");
+        Li.innerHTML= tet.value;
+        tet_li.appendChild(Li);
+  }
+       </script>
